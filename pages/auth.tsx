0b800e87e1615674cef092f3a52b4f55b009dee6
Rajpreet0 <rajpreet.singh@gmx.de>
2024-02-24 18:02:11 +0100

    added auth page frontend

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,73 @@
-const Auth = () => {
-    return(
-        <div className="">
+import Input from "@/components/Input";
+import { useCallback, useState } from "react";
 
+const Auth = () => {
+
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    
+    /* ---- Variant Toggle Method to switch between Login and SignUp ----*/
+    
+    // At default it should open Login Screen
+    const [variant, setVariant] = useState('Login');
+
+    const toggleVariant = useCallback(() => {
+        // If currentVariant is equal to Login it should toggel to register otherwise it should toggle to login
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+    }, [])
+
+    return(
+        // bg-[] => Custom Background inside putting a url('relative_path_to_image')
+        <div className="relative h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo.png" alt="Logo" className="h-12"></img>
+                </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            {/* Using Ternity Operator it is possible to show different UI compoents when on Login or Register  */}
+                            {variant === 'login' ? 'Sign In' : 'Register'}
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            {variant === 'register' && (
+                                <Input 
+                                    label="Username" 
+                                    onChange={(e: any) => {setName(e.target.value)}} 
+                                    id="name" 
+                                    value={name} 
+                                />
+                            )}
+                            <Input 
+                                label="Email" 
+                                onChange={(e: any) => {setEmail(e.target.value)}} 
+                                id="email" 
+                                type="email" 
+                                value={email} 
+                            />
+                            <Input 
+                                label="Password" 
+                                onChange={(e: any) => {setPassword(e.target.value)}} 
+                                id="password" 
+                                type="password" 
+                                value={password} 
+                            />
+                        </div>
+                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            {variant === 'login' ? 'Login' : 'Sign Up'}
+                        </button>
+                        <p className="text-neutral-500 mt-12">
+                            {variant === 'login' ? 'First time using Netflix ?' : 'Already have an account?'}
+                            <span 
+                            onClick={toggleVariant}
+                            className="text-white ml-1 hover:underline cursor-pointer">
+                                 {variant === 'login' ? 'Create an Account' : 'Login'}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
